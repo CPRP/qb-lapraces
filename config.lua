6493b7202ac4d3fe810e3aad09a75d105d324608
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-10-12 20:35:16 -0400

    added WhitelistedCreators

--- config.lua
+++ config.lua
@@ -1,7 +1,13 @@
 Config = Config or {}
 
 Config.WhitelistedCreators = {
-    "PUTCID",
+    "FJK93736", -- Ben Wexler
+    "QBB71351", -- Coltron Scott
+    "PPI19251", -- Ellie Martinez
+    "CQA47822", -- Brandin Martinez
+    "RIU41318", -- Andrew Smith
+    "UMJ82839", -- Firoza Green
+    "QOK28309", -- Jessi Westin
 }
 
 Config.RaceSetupAllowed = true
